db9c395ae7a97a7cd3c505b5b6d219427901a577
Thiago Martins de Sousa <thiagodf@live.com>
2017-06-29 18:47:35 -0300

    Dependency Injection

---
+++ BushiApp.Startup/DependencyResolver.cs
@@ -0,0 +1,22 @@
+﻿using BushiApp.Domain.Contracts.Repositories;
+using BushiApp.Domain.Models;
+using BushiApp.Infraestructure.Data;
+using BushiApp.Infraestructure.Repositories;
+using Microsoft.Practices.Unity;
+
+namespace BushiApp.Startup
+{
+    public static class DependencyResolver 
+    {
+        public static void Resolve(UnityContainer container)
+        {
+            container.RegisterType<AppDataContext, AppDataContext>(new HierarchicalLifetimeManager());
+            container.RegisterType<IModalidadeRepositorio, ModalidadeRepositorio>(new HierarchicalLifetimeManager());
+            container.RegisterType<IProfessorRepositorio, ProfessorRepositorio>(new HierarchicalLifetimeManager());
+
+            container.RegisterType<Professor, Professor>(new HierarchicalLifetimeManager());
+            container.RegisterType<Modalidade, Modalidade>(new HierarchicalLifetimeManager());
+        }
+
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,8 @@
 ﻿using BushiApp.Domain.Contracts.Repositories;
-using BushiApp.Domain.Models;
 using BushiApp.Infraestructure.Data;
 using BushiApp.Infraestructure.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BushiApp.Startup;
+using Microsoft.Practices.Unity;
 
 namespace ConsoleApp1
 {
@@ -15,8 +11,15 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             // Verificar se o relacionamento de professor com modalidade realmente é N pra N ou 1 pra N.
+            var container = new UnityContainer();
+            DependencyResolver.Resolve(container);
+            var modalidadeRep = container.Resolve<ModalidadeRepositorio>();
+            var modRep = modalidadeRep.Get("Jiu-Jitsu");
 
-            AppDataContext contexto = new AppDataContext();
+            //IModalidadeRepositorio modRep;
+
+            //var modsRep = modRep.Get("Jiu-Jitsu");
+
             //var modalidade = new Modalidade(
             //    "Jiu-Jitsu",
             //    "Arte Marcial Suave"
@@ -27,20 +30,17 @@ namespace ConsoleApp1
             //    modalidadeRep.Create(modalidade);
             //}
 
-            using (IModalidadeRepositorio modalidadeRep = new ModalidadeRepositorio(contexto))
-            {
-                var modRep = modalidadeRep.Get("Jiu-Jitsu");
-                List<Modalidade> modalidades = new List<Modalidade>();
-                modalidades.Add(modRep);
+            //using (IModalidadeRepositorio modalidadeRep = new ModalidadeRepositorio(contexto))
+            //{
+            //    var modRep = modalidadeRep.Get("Jiu-Jitsu");
+            //    List<Modalidade> modalidades = new List<Modalidade>();
+            //    modalidades.Add(modRep);
 
-                var professor = new Professor("Thiago", modalidades);
-                contexto = new AppDataContext();
-                IProfessorRepositorio profRep = new ProfessorRepositorio(contexto);
-                profRep.Create(professor);
-
-
-            
-            }
+            //    var professor = new Professor("Thiago", modalidades);
+            //    contexto = new AppDataContext();
+            //    IProfessorRepositorio profRep = new ProfessorRepositorio(contexto);
+            //    profRep.Create(professor);
+            //}
         }
     }
 }
