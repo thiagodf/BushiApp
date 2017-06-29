ba7c94251badfc34134a6213b9406d27b38d8209
Thiago Martins de Sousa <thiagodf@live.com>
2017-06-29 10:44:37 -0300

    Ajuste

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using BushiApp.Domain.Contracts.Repositories;
 using BushiApp.Domain.Models;
+using BushiApp.Infraestructure.Data;
 using BushiApp.Infraestructure.Repositories;
 using System;
 using System.Collections.Generic;
@@ -13,22 +14,33 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            // Verificar se o relacionamento de professor com modalidade realmente é N pra N ou 1 pra N.
+
+            AppDataContext contexto = new AppDataContext();
             //var modalidade = new Modalidade(
-            //    "Jiu-Jitsu", 
+            //    "Jiu-Jitsu",
             //    "Arte Marcial Suave"
             //    );
 
-            //using (IModalidadeRepositorio modalidadeRep = new ModalidadeRepositorio())
+            //using (IModalidadeRepositorio modalidadeRep = new ModalidadeRepositorio(contexto))
             //{
             //    modalidadeRep.Create(modalidade);
             //}
 
-            //using (IModalidadeRepositorio modalidadeRep = new ModalidadeRepositorio())
-            //{
-            //    var modRep = modalidadeRep.Get("Jiu-Jitsu");
-            //    Console.WriteLine(modRep.Descricao);
-            //    Console.ReadKey();
-            //}
+            using (IModalidadeRepositorio modalidadeRep = new ModalidadeRepositorio(contexto))
+            {
+                var modRep = modalidadeRep.Get("Jiu-Jitsu");
+                List<Modalidade> modalidades = new List<Modalidade>();
+                modalidades.Add(modRep);
+
+                var professor = new Professor("Thiago", modalidades);
+                contexto = new AppDataContext();
+                IProfessorRepositorio profRep = new ProfessorRepositorio(contexto);
+                profRep.Create(professor);
+
+
+            
+            }
         }
     }
 }
